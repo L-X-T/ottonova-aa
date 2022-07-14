269b92ac4ae5f68699dd5b99bf47730b9937a1d0
Alex Thalhammer <alex@angular-software.at>
2022-07-14 10:24:18 +0200

    [RxJS] Bonus: Add basic Error Handling

--- libs/flight-lib/src/lib/services/airport.service.ts
+++ libs/flight-lib/src/lib/services/airport.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -11,6 +12,6 @@ export class AirportService {
   constructor(private httpClient: HttpClient) {}
 
   findAll(): Observable<string[]> {
-    return this.httpClient.get<string[]>(this.apiUrl);
+    return this.httpClient.get<string[]>(this.apiUrl).pipe(map((airports) => []));
   }
 }
