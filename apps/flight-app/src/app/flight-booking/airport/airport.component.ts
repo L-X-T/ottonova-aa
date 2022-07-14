d5be4601a4abdfbf6c5be546d8a1dbb97a0bab94
Alex Thalhammer <alex@angular-software.at>
2022-07-14 09:50:57 +0200

    [RxJS] 1. Unsubscribe with Subscription

--- apps/flight-app/src/app/flight-booking/airport/airport.component.ts
+++ apps/flight-app/src/app/flight-booking/airport/airport.component.ts
@@ -1,19 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { AirportService } from '@flight-workspace/flight-lib';
+import { Observable, Observer, Subscription } from 'rxjs';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'flight-workspace-airport',
   templateUrl: './airport.component.html',
   styleUrls: ['./airport.component.css']
 })
-export class AirportComponent implements OnInit {
+export class AirportComponent implements OnInit, OnDestroy {
+  airports$: Observable<string[]>;
+
+  // 1 using subscription & unsubscribe
   airports: string[] = [];
+  private airportsObserver: Observer<string[]>;
+  private airportsSubscription: Subscription;
 
   constructor(private airportService: AirportService) {}
 
   ngOnInit(): void {
-    this.airportService.findAll().subscribe((airports) => {
-      this.airports = airports;
-    });
+    this.airports$ = this.airportService.findAll();
+
+    this.airportsObserver = {
+      next: (airports) => this.onLoadAirportsSuccessfully(airports),
+      error: (err) => this.onLoadAirportsFail(err),
+      complete: () => {
+        console.warn('airports$ completed');
+      }
+    };
+
+    // 1 using subscription & unsubscribe
+    this.airportsSubscription = this.airports$.subscribe(this.airportsObserver);
+  }
+
+  ngOnDestroy(): void {
+    // 1 using subscription & unsubscribe
+    this.airportsSubscription?.unsubscribe();
+  }
+
+  private onLoadAirportsSuccessfully(airports: string[]): void {
+    console.log('airports$ next: ' + airports);
+    this.airports = airports;
+  }
+
+  private onLoadAirportsFail(err: HttpErrorResponse): void {
+    console.error('airports$ error: ' + err);
   }
 }
